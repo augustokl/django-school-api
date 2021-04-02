5e61ae31f6cbe21ee9d02b2e22dee9e299b884b5
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-04-02 16:00:30 -0300

    Initial commit, create school app

---
+++ school/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Student(models.Model):
+  name = models.CharField(max_length=30)
+  rg = models.CharField(max_length=9)
+  cpf = models.CharField(max_length=11)
+  birthday = models.DateField()
+
+  def __str__(self):
+      return self.name
+  
+
+class Course(models.Model):
+  LEVEL = (
+    ('B', 'Basic'),
+    ('I', 'Intermediate'),
+    ('A', 'Advanced'),
+  )
+  code = models.CharField(max_length=10)
+  description = models.CharField(max_length=100)
+  code = models.CharField(max_length=10)
+  level = models.CharField(max_length=1, choices=LEVEL, blank=False, default='B')
+
+  def __str__(self):
+      return self.description
+  
